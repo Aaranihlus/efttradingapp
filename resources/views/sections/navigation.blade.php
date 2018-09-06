bf679b2fee9eba6ed1721b49d3653acb3cfdb69d
Aaranihlus <NathanL38@hotmail.com>
2018-09-06 10:16:12 +0100

    Complete Restyle of front end. New Fonts.

--- resources/views/sections/navigation.blade.php
+++ resources/views/sections/navigation.blade.php
@@ -4,7 +4,7 @@
   <!-- Light #fef7db -->
 
   <div class="container">
-  <a class="navbar-brand" href="/">EFT Market</a>
+  <a class="navbar-brand bold" href="/">EFT Market</a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
